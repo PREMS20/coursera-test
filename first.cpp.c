a50b5e0b5f1a92a717ac8a5620c6fac16dafec03
PREMS20 <84615370+PREMS20@users.noreply.github.com>
2021-06-05 12:32:53 +0530

    Add files via upload
    hello world

---
+++ first.cpp.c
@@ -0,0 +1,18 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+int a;
+int b;
+int sum;
+cout<<"\n enter the first no";
+cin>>a;
+cout<<"\n enter the second number";
+cin>>b;
+sum=a+b;
+cout<<"\n sum";
+return 0;
+
+
+
+}
